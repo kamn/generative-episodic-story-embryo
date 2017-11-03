013fffa120500e61f20690e4c9731657af4148b4
Samuel Miller <kamn@users.noreply.github.com>
2017-11-02 19:54:02 -0600

    Corrected the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject generative-episodic-story-embryo "0.0.4"
+(defproject generative-episodic-story-embryo "0.1.0"
   :description "A story generator for NaNoGenMo 2017"
   :url "https://github.com/kamn/generative-episodic-story-embryo"
   :license {:name "Eclipse Public License"
